03c487db946fbf7bb8a41a3e22b2b0eaebc63599
Amine Trigui <aminetrigui.github@gmail.com>
2024-01-25 16:36:58 +0100

    add value by input

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 import Counter from "./Counter";
 import Product from "./Product";
+import { v4 as uuid } from 'uuid';
 
 function Products() {
-  let title = "Learn How to create apps with reactjs 2023";
   let showList = true;
+
+const [title, setTitle]= useState("");
+const [price, setPrice]= useState("");
+
+const submitForm = (e) => { e.preventDefault()  
+  
+  let myProduct = {
+    id: uuid()  ,
+    label: title,
+    price
+   }
+   setProducts( [myProduct, ...products ] )
+  }
+
+let titleInput = (e) => { setTitle(e.target.value)  }
+let priceInput = (e) => { setPrice(e.target.value)  }
+
 
   const [products, setProducts] = useState([
     {
@@ -28,9 +45,14 @@ function Products() {
     let mylist = products.filter((pr) => pr.id !== id);
      setProducts(mylist);
   };
+
+
+
+
+
   return (
     <div>
-      <h1>{title}</h1>
+        {/* <h1>{title}</h1> */}
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore et quae
         nesciunt adipisci, temporibus corrupti deserunt placeat libero assumenda
@@ -38,6 +60,17 @@ function Products() {
         perspiciatis facere.
       </p>
 
+{title} 👍🐧🐧🎉{price}
+<form action="" onSubmit={submitForm}>
+<div>
+<label htmlFor="">Title</label>
+<input onChange={titleInput} className="form-control my-2 " type="text" />
+
+<label htmlFor="">Price</label>
+<input onChange={priceInput} className="form-control my-2 " type="text" />
+</div>
+<button className="btn btn-info my-2"> Save </button>
+</form>
       <Counter />
 
       {showList && (
